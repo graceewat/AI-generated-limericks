13251822c7df29036f3b6c93eebc269d75fa1f37
graceewat <160866251+graceewat@users.noreply.github.com>
2024-07-17 15:23:16 -0400

    added basic javascript form handling and typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-
+function generatePoem(event) {
+  event.preventDefault();
+  new Typewriter("#poem", {
+    strings: ["Poem to be written here"],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+let formElement = document.querySelector("#form-generator");
+formElement.addEventListener("submit", generatePoem);
