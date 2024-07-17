24efb4e3308bf771a2acc2fa72deedf9a9ea4cb4
graceewat <160866251+graceewat@users.noreply.github.com>
2024-07-17 17:25:34 -0400

    Improved js prompt and added waiting message

--- src/index.js
+++ src/index.js
@@ -14,8 +14,13 @@ function generatePoem(event) {
   let apiKey = "f03c7t04000f0dbod331aeefa47ad1f6";
   let prompt = `Please generate a funny limerick using ${limerickInput.value} as the name of the subject`;
   let context =
-    "You are an expert in writing limericks and love to write funny ones based around peoples names. Your mission is to generate a 5 line limerick in basic HTML. The limerick should follow a traditional rhyming format.Do not include a title to the limerick or any speechmarks.";
+    "You are an expert in writing limericks and love to write funny ones based around peoples names. Your mission is to generate a 5 line limerick in basic HTML with a <br /> after each line. The limerick should follow a traditional rhyming pattern. There should not be any speech marks or titles.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  let poemElement = document.querySelector(".poem");
+
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `Generating a limerick for ${limerickInput.value}...`;
+
   console.log("generating limerick");
   console.log(`prompt:${prompt}`);
   console.log(`context:${context}`);
